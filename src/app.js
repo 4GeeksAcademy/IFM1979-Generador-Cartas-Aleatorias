53b6b2a7a8c4cc9d92d26e438ca47661414f0d55
Iván Fuentes Mahíllo <169067125+IFM1979@users.noreply.github.com>
2024-07-22 14:57:30 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+// Botón recarga la página al hacer click
+const reloadButton = document.getElementById("reloadButton");
+reloadButton.addEventListener("click", () => {
+  location.reload();
+});
+
 let suitsCards = ["♦", "♥", "♠", "♣"];
 let numbersCards = [
   "A",
@@ -16,13 +22,14 @@ let numbersCards = [
   "8",
   "9",
   "10",
+  "K",
   "Q",
-  "J",
-  "K"
+  "J"
 ];
 
 window.onload = function() {
   //write your code her
+
   let suitsCardsRamdom =
     suitsCards[Math.floor(Math.random() * suitsCards.length)];
   let numbersCardsRamdom =
@@ -38,3 +45,10 @@ window.onload = function() {
 
   document.querySelector(".number").innerHTML = numbersCardsRamdom;
 };
+/*
+let newWidth = prompt("Enter a width for your card (e.g., 200px, 50%)");
+let newHeight = prompt("Enter a height for your card (e.g., 300px, 75%)");
+
+let selectionCard = document.querySelector(".carta");
+selectionCard.style.width = newWidth;
+selectionCard.style.height = newHeight;*/
