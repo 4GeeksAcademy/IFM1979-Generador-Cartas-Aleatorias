6e8f7ed2c260f4be38a0c29cea280f9a474544bd
Iván Fuentes Mahíllo <169067125+IFM1979@users.noreply.github.com>
2024-07-22 13:11:38 +0000

    Cambios HTML Y CSS 2

--- src/app.js
+++ src/app.js
@@ -4,8 +4,37 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let suitsCards = ["♦", "♥", "♠", "♣"];
+let numbersCards = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Q",
+  "J",
+  "K"
+];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code her
+  let suitsCardsRamdom =
+    suitsCards[Math.floor(Math.random() * suitsCards.length)];
+  let numbersCardsRamdom =
+    numbersCards[Math.floor(Math.random() * numbersCards.length)];
+
+  let selectorSuits = document.querySelectorAll(".suits");
+  for (let element of selectorSuits) {
+    element.innerHTML = suitsCardsRamdom;
+    if (suitsCardsRamdom === "♦" || suitsCardsRamdom === "♥") {
+      element.style.color = "red";
+    }
+  }
+
+  document.querySelector(".number").innerHTML = numbersCardsRamdom;
 };
